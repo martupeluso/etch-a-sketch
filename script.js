413393ef4a95a0ce913ebf454c2f0fb11b446d2b
Martina Peluso <185727137+martupeluso@users.noreply.github.com>
2025-03-22 19:49:11 +0000

    feat: add button to change the grid's size

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const button = document.querySelector("button");
+
+button.addEventListener("click", () => {
+    let squaresPerSide;
+    do {
+        squaresPerSide = prompt("How many squares per side should the grid have? (1-100)");
+    } while (squaresPerSide <= 0 || squaresPerSide > 100);
+})
+
 const container = document.querySelector(".container");
 
 for (let i = 0; i < 256; i++){
